3f09a0b30932dbf8537fe88429a67e437f199809
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-10-03 13:44:08 +0200

    Delete AuthenticationProvider.java

--- backend/chatapp/src/main/java/com/tmploeg/chatapp/security/AuthenticationProvider.java
+++
@@ -1,6 +0,0 @@
-package com.tmploeg.chatapp.security;
-
-import com.tmploeg.chatapp.users.User;
-import java.util.function.Supplier;
-
-public interface AuthenticationProvider extends Supplier<User> {}
