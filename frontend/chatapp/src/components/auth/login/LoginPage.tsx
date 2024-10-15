c39a4de3e481c3c0ae90bd103b330769434744fe
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-10-15 10:24:39 +0200

    added login form validation

--- frontend/chatapp/src/components/auth/login/LoginPage.tsx
+++ frontend/chatapp/src/components/auth/login/LoginPage.tsx
@@ -4,7 +4,8 @@ import "../Auth.css";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../hooks";
 import { Form as FormikForm, Formik } from "formik";
-import { Form, InputGroup } from "react-bootstrap";
+import { Button, Form, InputGroup } from "react-bootstrap";
+import { LoginSchema } from "../AuthSchemas";
 
 interface Props {
   onLogin?: () => void;
@@ -19,39 +20,57 @@ export default function LoginPage({ onLogin }: Props) {
 
   return (
     <div className="auth-page">
-      <Formik
-        initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => console.log(values.username)}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleSubmit,
-          handleBlur,
-          isSubmitting,
-        }) => (
-          <div>
-            <FormikForm className="auth-form">
-              {/* <div className="auth-form"> */}
-              <Form.Control
-                name="username"
-                value={values.username}
-                onChange={handleChange}
-                placeholder="username"
-              />
-              <Form.Control
-                name="password"
-                value={values.password}
-                onChange={(e) => handleChange(e)}
-                placeholder="password"
-              />
-              {/* </div> */}
-            </FormikForm>
-          </div>
-        )}
-      </Formik>
+      <div className="auth-form-container">
+        <Formik
+          initialValues={{ username: "", password: "" }}
+          onSubmit={(values) => console.log(values.username)}
+          validationSchema={LoginSchema}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleSubmit,
+            handleBlur,
+            isSubmitting,
+          }) => (
+            <div>
+              <FormikForm onSubmit={handleSubmit} className="auth-form">
+                <InputGroup>
+                  <Form.Control
+                    name="username"
+                    value={values.username}
+                    onChange={handleChange}
+                    placeholder="username"
+                    isInvalid={touched.username && !!errors.username}
+                    onBlur={handleBlur}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.username}
+                  </Form.Control.Feedback>
+                </InputGroup>
+                <InputGroup>
+                  <Form.Control
+                    name="password"
+                    value={values.password}
+                    onChange={(e) => handleChange(e)}
+                    placeholder="password"
+                    isInvalid={touched.password && !!errors.password}
+                    onBlur={handleBlur}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.password}
+                  </Form.Control.Feedback>
+                </InputGroup>
+                <Button type="submit" disabled={isSubmitting} size="lg">
+                  Login
+                </Button>
+              </FormikForm>
+            </div>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 
