e62bd35384b4157b342087aada50b4322d20af38
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-10-15 11:02:45 +0200

    improved login styling

--- frontend/chatapp/src/components/auth/login/LoginPage.tsx
+++ frontend/chatapp/src/components/auth/login/LoginPage.tsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import { Auth } from "../../../models";
-import "../Auth.css";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../hooks";
 import { Form as FormikForm, Formik } from "formik";
 import { Button, Form, InputGroup } from "react-bootstrap";
 import { LoginSchema } from "../AuthSchemas";
 import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
+import "../Auth.css";
 
 interface Props {
   onLogin?: () => void;
@@ -22,13 +21,16 @@ export default function LoginPage({ onLogin }: Props) {
       <div className="auth-form-container">
         <Formik
           initialValues={{ username: "", password: "" }}
-          onSubmit={(values) => {
+          onSubmit={(values, { setSubmitting }) => {
             login(values)
               .then(() => {
                 onLogin?.();
                 navigate("/");
               })
-              .catch(() => alert("login failed"));
+              .catch(() => {
+                alert("login failed");
+                setSubmitting(false);
+              });
           }}
           validationSchema={LoginSchema}
         >
@@ -43,20 +45,22 @@ export default function LoginPage({ onLogin }: Props) {
           }) => {
             return (
               <div>
+                <div className="auth-form-title">Login</div>
                 <FormikForm onSubmit={handleSubmit} className="auth-form">
-                  <InputGroup>
+                  <InputGroup hasValidation>
                     <Form.Control {...getCommonProps("username")} />
                     <Form.Control.Feedback type="invalid">
                       {errors.username}
                     </Form.Control.Feedback>
                   </InputGroup>
-                  <InputGroup>
+                  <InputGroup hasValidation>
                     <Form.Control
                       {...getCommonProps("password")}
                       type={passwordVisible ? "text" : "password"}
                     />
                     <Button
                       onClick={() => setPasswordVisible((visible) => !visible)}
+                      size="lg"
                     >
                       {passwordVisible ? <BsEyeSlashFill /> : <BsEyeFill />}
                     </Button>
