21b7deff759407c7c7f12f312c77a8310d830c56
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-10-17 14:58:18 +0200

    updated useWebSocket

--- frontend/chatapp/src/hooks/useWebSocket.ts
+++ frontend/chatapp/src/hooks/useWebSocket.ts
@@ -1,43 +1,36 @@
 import {
   ActivationState,
   Client,
-  CompatClient,
-  IMessage,
   Stomp,
   StompSubscription,
 } from "@stomp/stompjs";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import useStorage from "./useStorage";
 import { StorageLocation } from "../enums/StorageLocation";
 import { JWT } from "../models/auth";
 
-const AUTH_HEADER_NAME = "Authorization";
+const AUTH_HEADER_NAME: string = "Authorization";
+const HEARTBEAT_INTERVAL_DELAY: number = 180000;
+const APP_DESTINATION = "/app";
 
 export interface WebSocketConfig {
   enableDebug?: boolean;
+  enableHeartbeat?: boolean;
+  onConnect: () => void;
+  onDisconnect: () => void;
 }
 export default function useWebSocket(config?: WebSocketConfig): Socket {
-  const [stompClient] = useState<Client>(
-    getClient(config?.enableDebug ?? false)
-  );
+  const [stompClient] = useState<Client>(getClient(config));
+
   const { get: getJWT } = useStorage<JWT>(StorageLocation.JWT);
 
   function connect() {
-    const jwt = getJWT();
-    if (!jwt) {
-      console.error("auth token missing");
+    if (stompClient.state !== ActivationState.INACTIVE) {
+      console.warn("cannot connect: already connected");
       return;
     }
 
-    const headers: any = {};
-    headers[AUTH_HEADER_NAME] = jwt.token;
-
-    stompClient.configure({ connectHeaders: headers });
     stompClient.activate();
-  }
-
-  function onConnect(callback: () => void) {
-    stompClient.onConnect = callback;
   }
 
   function disconnect() {
@@ -49,22 +42,25 @@ export default function useWebSocket(config?: WebSocketConfig): Socket {
     stompClient.deactivate();
   }
 
-  function onDisconnect(callBack: () => void) {
-    stompClient.onDisconnect = callBack;
-  }
-
   function send(destination: string, content: string) {
     if (stompClient.state !== ActivationState.ACTIVE) {
-      console.error("cannot send message when not connected");
-      return;
+      throw new Error("cannot send message when not connected");
     }
-    stompClient.publish({ destination: `/app/${destination}`, body: content });
+
+    stompClient.publish({
+      destination: APP_DESTINATION + destination,
+      body: content,
+    });
   }
 
   function subscribe<TMessage>(
     destination: string,
     callback: (message: TMessage) => void
   ) {
+    if (stompClient.state !== ActivationState.ACTIVE) {
+      throw new Error("cannot subscribe when not connected");
+    }
+
     return stompClient.subscribe(destination, (message) =>
       callback(JSON.parse(message.body))
     );
@@ -72,32 +68,47 @@ export default function useWebSocket(config?: WebSocketConfig): Socket {
 
   return {
     connect,
-    onConnect,
     disconnect,
-    onDisconnect,
     send,
     subscribe,
   };
+
+  function getClient(config?: WebSocketConfig): Client {
+    const client = Stomp.client(import.meta.env.VITE_WEBSOCKET_URL);
+
+    if (!config?.enableDebug) {
+      client.debug = () => {};
+    }
+
+    const jwt = getJWT();
+    if (!jwt) {
+      throw new Error("auth token missing");
+    }
+
+    const headers: any = {};
+    headers[AUTH_HEADER_NAME] = jwt.token;
+
+    client.configure({
+      connectHeaders: headers,
+      heartbeatOutgoing: HEARTBEAT_INTERVAL_DELAY,
+      onConnect: () => {
+        config?.onConnect?.();
+      },
+      onDisconnect: () => {
+        config?.onDisconnect?.();
+      },
+    });
+
+    return client;
+  }
 }
 
 export interface Socket {
   connect: () => void;
-  onConnect: (callBack: () => void) => void;
   disconnect: () => void;
-  onDisconnect: (callBack: () => void) => void;
   send: (destination: string, content: string) => void;
   subscribe: <T>(
     destination: string,
     callback: (message: T) => void
   ) => StompSubscription;
-}
-
-function getClient(enableDebug: boolean): Client {
-  const client = Stomp.client(import.meta.env.VITE_WEBSOCKET_URL);
-
-  if (!enableDebug) {
-    client.debug = () => {};
-  }
-
-  return client;
 }
