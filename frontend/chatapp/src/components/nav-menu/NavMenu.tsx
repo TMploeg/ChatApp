33f67710c5a0fe054071e722e9f75b902e8ca352
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-10-17 12:29:08 +0200

    updated nav menu

--- frontend/chatapp/src/components/nav-menu/NavMenu.tsx
+++ frontend/chatapp/src/components/nav-menu/NavMenu.tsx
@@ -1,17 +1,21 @@
 import { Nav } from "react-bootstrap";
 import "./NavMenu.scss";
 import { BsGlobe, BsHouseFill } from "react-icons/bs";
+import { useAppNavigate } from "../../hooks";
+import AppRoute from "../../enums/AppRoute";
 
 const NAV_ICON_SIZE = 45;
 export default function NavMenu() {
+  const navigate = useAppNavigate();
+
   return (
     <div className="nav-menu">
       <Nav>
-        <Nav.Item>
+        <Nav.Item onClick={() => navigate(AppRoute.HOME)}>
           <BsHouseFill size={NAV_ICON_SIZE} />
           <span>Home</span>
         </Nav.Item>
-        <Nav.Item>
+        <Nav.Item onClick={() => navigate(AppRoute.GLOBAL_CHAT)}>
           <BsGlobe size={NAV_ICON_SIZE} />
           <span>Global Chat</span>
         </Nav.Item>
