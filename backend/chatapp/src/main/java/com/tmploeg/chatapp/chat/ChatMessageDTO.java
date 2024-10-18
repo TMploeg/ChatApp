3109a4dc28635f7d56f67432fe82469008633135
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-10-18 15:44:01 +0200

    Update ChatMessageDTO.java

--- backend/chatapp/src/main/java/com/tmploeg/chatapp/chat/ChatMessageDTO.java
+++ backend/chatapp/src/main/java/com/tmploeg/chatapp/chat/ChatMessageDTO.java
@@ -1,7 +1,8 @@
 package com.tmploeg.chatapp.chat;
 
-public record ChatMessageDTO(String content, String sender) {
+public record ChatMessageDTO(String content, String sender, String sendAt) {
   public static ChatMessageDTO from(ChatMessage message) {
-    return new ChatMessageDTO(message.getContent(), message.getSender().getUsername());
+    return new ChatMessageDTO(
+        message.getContent(), message.getSender().getUsername(), message.getSendAt().toString());
   }
 }
