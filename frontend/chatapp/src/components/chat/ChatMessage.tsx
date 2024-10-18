7e7fdfebb0f462d8bf7ac5451a586794b11a83ad
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-10-18 15:59:03 +0200

    added usernames to text messages

--- frontend/chatapp/src/components/chat/ChatMessage.tsx
+++ frontend/chatapp/src/components/chat/ChatMessage.tsx
@@ -6,8 +6,11 @@ interface Props {
 }
 export default function ChatMessage({ message, isOwned }: Props) {
   return (
-    <div className={`chat-message ${isOwned ? "owned" : null}`}>
-      {message.content}
+    <div className={`chat-message ${isOwned ? "owned" : ""}`}>
+      {!isOwned && (
+        <div className="chat-message-username">{message.sender}</div>
+      )}
+      <div className="chat-message-content">{message.content}</div>
     </div>
   );
 }
