67e41d5f76fa26a4cfb28c76f0a1b41d23f7a414
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-11-22 15:36:21 +0100

    removed chatgroups from seeder

--- backend/chatapp/src/main/java/com/tmploeg/chatapp/Seeder.java
+++ backend/chatapp/src/main/java/com/tmploeg/chatapp/Seeder.java
@@ -1,11 +1,8 @@
 package com.tmploeg.chatapp;
 
-import com.tmploeg.chatapp.chat.chatgroup.ChatGroup;
 import com.tmploeg.chatapp.chat.chatgroup.ChatGroupRepository;
 import com.tmploeg.chatapp.users.User;
 import com.tmploeg.chatapp.users.UserRepository;
-import java.util.List;
-import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -21,7 +18,6 @@ public class Seeder implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
     seedUsers();
-    seedChatGroups();
   }
 
   private void seedUsers() {
@@ -35,26 +31,5 @@ public class Seeder implements CommandLineRunner {
 
       userRepository.save(new User(username, password));
     }
-  }
-
-  private void seedChatGroups() {
-    List<User> users = userRepository.findAll();
-
-    chatGroupRepository.save(
-        new ChatGroup("Awesome group 1", Set.of(users.get(0), users.get(3), users.get(4))));
-    chatGroupRepository.save(
-        new ChatGroup("Awesome group 2", Set.of(users.get(0), users.get(1), users.get(2))));
-    chatGroupRepository.save(
-        new ChatGroup("Awesome group 3", Set.of(users.get(1), users.get(2), users.get(3))));
-    chatGroupRepository.save(
-        new ChatGroup("Awesome group 4", Set.of(users.get(2), users.get(3), users.get(4))));
-    chatGroupRepository.save(
-        new ChatGroup("Awesome group 5", Set.of(users.get(0), users.get(2), users.get(4))));
-
-    chatGroupRepository.save(new ChatGroup(Set.of(users.get(0), users.get(3))));
-    chatGroupRepository.save(new ChatGroup(Set.of(users.get(1), users.get(2))));
-    chatGroupRepository.save(
-        new ChatGroup(
-            "Awesome group 6", Set.of(users.get(0), users.get(2), users.get(3), users.get(4))));
   }
 }
