c051a86b5e7ab0cfcd7bba806d15c29935631672
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-12-01 16:16:01 +0100

    removed checkin

--- backend/chatapp/src/main/java/com/tmploeg/chatapp/checkin/CheckinDTO.java
+++
@@ -1,6 +0,0 @@
-package com.tmploeg.chatapp.checkin;
-
-import com.tmploeg.chatapp.connectionrequests.dtos.ConnectionRequestDTO;
-import java.util.List;
-
-public record CheckinDTO(List<ConnectionRequestDTO> newConnectionRequests) {}
